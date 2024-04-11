15db26d97b5ca266b05d5809c5f7d0bb9bc968b3
Lekyasri <lekyasri.c@technovert.com>
2024-04-11 12:22:15 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public static void main(String[] args)
+        {
+            int a = 1;
+            int b = 2;
+            Console.WriteLine(a + b);
+        }
+    }
+}
